f9ff97d877107d2d25ad85b76ab1c0c313ac7b07
upeguialex14 <nietoupeguialexander@gmail.com>
2025-08-14 11:37:33 -0500

    Frontend Backend basico

--- Back/src/db/DB.sql
+++ Back/src/db/DB.sql
@@ -2,6 +2,16 @@ CREATE DATABASE futbol_equipos;
 USE futbol_equipos;
 
 CREATE TABLE equipos(
-    id_equipo INT  PRIMARY KEY ,
-    nombre VARCHAR(100) NOT NULL
+    id_equipo INT  PRIMARY KEY AUTO_INCREMENT,
+    nombre_equipo VARCHAR(100)
 );   
+-- Insert = POST
+INSERT INTO equipos (nombre_equipo) VALUES
+    ("Alemania"),
+    ("Holanda"),
+    ("Suecia"),
+    ("Francia"),
+    ("Noruega");
+    
+-- Select = GET
+SELECT * FROM equipos;
